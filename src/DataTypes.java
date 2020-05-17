786979affefae1c9a4d0aedd6913e5146c43cd4f
Mariia Vynnyk <mariia.vynnyk@ctdev.io>
2020-05-17 19:39:38 +0300

    TheFirstHomeWork

---
+++ src/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+
+    static boolean b = true ;
+    static byte by = 2;
+    static char c = 'C';
+    static short s = 20;
+    static int i = 45;
+    static long l = 40L;
+    static float f = 25F;
+    static double d = 34.5;
+
+    public static void main(String[] args) {
+        System.out.println(b);
+        System.out.println(by);
+        System.out.println(c);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+
+    }
+
+}
