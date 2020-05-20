405370b459ed8af58350340fb4f7221d165e8d29
Mariia Vynnyk <mariia.vynnyk@ctdev.io>
2020-05-20 23:12:30 +0300

    changes

---
+++ src/Square.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Square {
+
+    public static void main(String[] args) {
+
+        int h = 5;
+        int a = 2;
+        int RectangleArea = h * a;
+
+        int radius = 4;
+        double CircleArea = Math.pow(radius,2)* Math.PI;
+
+        System.out.println("Area of Rectangle is: " + RectangleArea);
+        System.out.println("Area of Circle is: " + CircleArea);
+
+    }
+}
