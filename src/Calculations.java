26ccf1b553125d87d7d78e3c98984cd1136710a7
Mariia Vynnyk <mariia.vynnyk@ctdev.io>
2020-05-20 22:53:01 +0300

    1 changes

---
+++ src/Calculations.java
@@ -0,0 +1,14 @@
+public class Calculations {
+
+        public static void main(String[] args) {
+
+            int num1 = 45;
+            int num2 = 10;
+            int sum = num1 + num2;
+            int sub = num1 - num2;
+            int mul = num1 * num2;
+            int div = num1/num2;
+
+            System.out.println(sum + " " + sub + " " + mul + " " + div);
+    }
+}
