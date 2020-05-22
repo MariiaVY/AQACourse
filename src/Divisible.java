27fb6e0486ad9becb29f888473f3f34fc21c58f9
Mariia Vynnyk <mariia.vynnyk@ctdev.io>
2020-05-22 10:27:20 +0300

    changes

--- src/Divisible.java
+++ src/Divisible.java
@@ -3,7 +3,7 @@ public class Divisible {
     public static void main(String[] args) {
 
             for (int num = 1; num <= 100; num++) {
-                if ((num % 3 == 0 && num % 5 == 0) && num % 2 != 0) {
+                if ((num % 3 == 0 || num % 5 == 0) && num % 2 != 0) {
                     System.out.print(num + ", ");
                 }
             }
