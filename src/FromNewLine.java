0fac846f98b4675d3879e902dcb29f8b2d5ae6b4
Mariia Vynnyk <mariia.vynnyk@ctdev.io>
2020-05-30 21:09:02 +0300

    changes

--- src/FromNewLine.java
+++ src/FromNewLine.java
@@ -4,12 +4,11 @@ public class FromNewLine {
     public static void main(String[] args) {
 
         String names = "Den, John, Will, Kate, Adam, Robin";
-        System.out.println(names.substring(0, 3));
-        System.out.println(names.substring(5, 9));
-        System.out.println(names.substring(11, 15));
-        System.out.println(names.substring(17, 21));
-        System.out.println(names.substring(23, 27));
-        System.out.println(names.substring(29, 34));
+        String[] words = names.split(" ");
+        for(String w:words){
+            System.out.println(w);
+        }
     }
 }
+
 
