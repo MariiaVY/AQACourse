b97b3f21df8e0d63c0c05a7eccfb11812c44ae15
Mariia Vynnyk <mariia.vynnyk@ctdev.io>
2020-07-01 23:32:01 +0300

    changes

--- src/task2/Task.java
+++ src/task2/Task.java
@@ -1,3 +1,7 @@
+//2) Correct nine classes: Worker (employee), Clerk (clerk), IT (IT specialist), Programmer (programmer), ProjectManager (project manager), CTO (CTO), HR (recruiter), OfficeManager (office manager), Cleaner (cleaner).
+//Inherit a programmer, project manager and technical director from an IT specialist.
+//Inherit the recruiter, cleaner and office manager from the clerk.
+//Inherit the clerk and IT professional from the employee.
 package task2;
 
 public class Task {
